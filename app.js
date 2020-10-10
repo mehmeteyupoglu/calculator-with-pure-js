7deee87f8ad19f7e5409d035f803dede1e2e76f6
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-10-10 23:31:56 +0300

    Activate buttons

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-var inputValue = document.getElementById("value");
+addition.addEventListener('click', function () {
 
-window.onload = function () {
-    var result = 0
-  value.addEventListener("input", function() {
-      result = inputValue.value
-      return result
-  })
-      return result;
-}
+    if (!num1 && !num2) {
+        num1 = parseInt(inputValue1.value)
+        inputValue1.value = 0
+        document.getElementById("result").innerHTML = num1
+    } else {
+        num2 = parseInt(inputValue1.value)
+        inputValue1.value = 0 
+        document.getElementById("result").innerHTML = num1 + num2
+}    
+})
 
-for (let i = 0; i < operationButtons.length; i++){
-    operationButtons[i].addEventListener('click', function () {
-        alert("Hello World")
-    })
-}
+subtraction.addEventListener('click', function () {
+    if (!num1 && !num2) {
+        num1 = parseInt(inputValue1.value)
+        inputValue1.value = 0
+        document.getElementById("result").innerHTML = num1
+    } else {
+        num2 = parseInt(inputValue1.value)
+        inputValue1.value = 0 
+        document.getElementById("result").innerHTML = num1 - num2
+}    
+})
+
+multiplication.addEventListener('click', function () {
+
+    if (!num1 && !num2) {
+        num1 = parseInt(inputValue1.value)
+        inputValue1.value = 0
+        document.getElementById("result").innerHTML = num1
+    } else {
+        num2 = parseInt(inputValue1.value)
+        inputValue1.value = 0 
+        document.getElementById("result").innerHTML = num1 * num2
+}    
+})
+
+division.addEventListener('click', function () {
+
+    if (!num1 && !num2) {
+        num1 = parseInt(inputValue1.value)
+        inputValue1.value = 0
+        document.getElementById("result").innerHTML = num1
+    } else {
+        num2 = parseInt(inputValue1.value)
+        inputValue1.value = 0 
+        document.getElementById("result").innerHTML = num1 / num2
+}    
+})
+
+clear.addEventListener("click", function () {
+    num1 = 0
+    num2 = 0
+    document.getElementById("result").innerHTML = 0
+})
--- vendor.js
+++ vendor.js
@@ -1,17 +1,12 @@
-var inputValue = document.getElementById("value");
-window.onload = function() {
-  value.addEventListener("input", function() {
-    console.log(inputValue.value);
-  });
-}
-
 var addition = document.getElementById('addition')
-
 var subtraction = document.getElementById('subtraction') 
-
 var multiplication = document.getElementById('multiplication')
-
 var division = document.getElementById('division') 
+var clear = document.getElementById("clear")
 
 var operationButtons = document.getElementsByClassName("operation")
 
+var result = document.getElementById("result")
+var inputValue1 = document.getElementById("value-1")
+var inputValue2 = document.getElementById("value-2")
+var num1 , num2; 
